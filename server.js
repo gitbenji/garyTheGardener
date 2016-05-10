a83ae82f2f15962d5d743e173d4346ae91f33a15
Benji Shankwitz <benji@Benjis-MacBook-Pro.local>
2016-05-10 11:15:18 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var app = {};
+
+var express = require('express');
+var http = require('http');
+
+var port = 8080;
+
+var e = app.e = express();
+app.server = app.server = http.createServer(e);
+
+app.server.listen(port, function() {
+	console.log('Listening on port ' + port + '\n');
+});
+
+e.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === '<validation_token>') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
