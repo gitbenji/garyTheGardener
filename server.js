487c4b5719a56a6cfb7695ac81201ec268d510b2
Benji Shankwitz <benji@Benjis-MacBook-Pro.local>
2016-05-10 11:49:11 -0400

    okay it works

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.server.listen(port, function() {
 
 e.get('/webhook/', function (req, res) {
 
-  // if (req.query['hub.verify_token'] === '<validation_token>') {
-  //   res.send(req.query['hub.challenge']);
-  // }
+  if (req.query['hub.verify_token'] === '<validation_token>') {
+    res.send(req.query['hub.challenge']);
+  }
   res.send('Error, wrong validation token');
 })
